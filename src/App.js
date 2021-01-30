9b0c529e52afad8ed1335b1946c6f4a8d7e2ab99
hwanseok.dev <hwanseok.dev@gmail.com>
2021-01-30 22:56:17 +0900

    apply owner api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import Modal from "./components/Modal";
+import Modal from "./components/ModalTodo";
 import axios from "axios";
 
 class App extends Component {
@@ -12,26 +12,49 @@ class App extends Component {
                 description: "",
                 completed: false
             },
-            todoList: []
+            activeOwner: {
+                id: 1,
+                name: "default name",
+                join_date: "default_date"
+            },
+            modalItem : false,
+            modalOwner : false,
+            todoList: [],
+            ownerList: [],
         };
     }
 
     componentDidMount() {
-        this.refreshList();
+        this.refreshItemList();
+        this.refreshOwnerList();
     }
-
-    refreshList = () => {
+    /**
+     * todoList와 ownerList 모두 새롭게 갱신
+     */
+    refreshItemList = () => {
         axios
             .get("/api/todos/")
             .then(res => this.setState({todoList: res.data}))
             .catch(err => console.log(err));
     };
+    refreshOwnerList = () => {
+        axios
+            .get("/api/owners/")
+            .then(res => this.setState({ownerList: res.data}))
+            .catch(err => console.log(err));
+    };
+    /**
+     * todoList 중 completed와 Incompleted 중 어떤 것을 보여줄지 설정
+     */
     displayCompleted = status => {
         if (status) {
             return this.setState({viewCompleted: true});
         }
         return this.setState({viewCompleted: false});
     };
+    /**
+     * complete/Incomplete 버튼 출력
+     */
     renderTabList = () => {
         return (
             <div className="my-5 tab-list">
@@ -50,6 +73,9 @@ class App extends Component {
             </div>
         );
     };
+    /**
+     * todoList 중 completed 또는 Incompleted만 filter해서 <li>로 나열
+     * */
     renderItems = () => {
         const {viewCompleted} = this.state;
         const newItems = this.state.todoList.filter(
@@ -86,38 +112,93 @@ class App extends Component {
             </li>
         ));
     };
-    toggle = () => {
-        this.setState({modal: !this.state.modal});
+    /**
+     * todoList 중 completed 또는 Incompleted만 filter해서 <li>로 나열
+     * */
+    renderOwners = () => {
+        return this.state.ownerList.map(item => (
+            <li
+                key={item.id}
+                className="list-group-item d-flex justify-content-between align-items-center"
+            >
+            <span
+                className={`todo-title mr-2 ${
+                    this.state.viewCompleted ? "completed-todo" : ""
+                }`}
+                title={item.join_date}
+            >
+              {item.name}
+            </span>
+                <span>
+              <button
+                  onClick={() => this.editItem(item)}
+                  className="btn btn-secondary mr-2"
+              >
+                {" "}
+                  Edit{" "}
+              </button>
+              <button
+                  onClick={() => this.handleDelete(item)}
+                  className="btn btn-danger"
+              >
+                Delete{" "}
+              </button>
+            </span>
+            </li>
+        ));
     };
-    handleSubmit = item => {
-        this.toggle();
+    toggleItem = () => {
+        this.setState({modalItem: !this.state.modalItem});
+    };
+    toggleOwner = () => {
+        this.setState({modalOwner: !this.state.modalOwner});
+    };
+    handleSubmitItem = item => {
+        this.toggleItem();
         if (item.id) {
             axios
                 .put(`http://localhost:8000/api/todos/${item.id}/`, item)
-                .then(res => this.refreshList());
+                .then(res => this.refreshItemList());
             return;
         }
         axios
             .post("http://localhost:8000/api/todos/", item)
-            .then(res => this.refreshList());
+            .then(res => this.refreshItemList());
+    };
+    handleSubmitOwner = item => {
+        this.toggleOwner();
+        if (item.id) {
+            axios
+                .put(`http://localhost:8000/api/owners/${item.id}/`, item)
+                .then(res => this.refreshOwnerList());
+            return;
+        }
+        axios
+            .post("http://localhost:8000/api/owners/", item)
+            .then(res => this.refreshOwnerList());
     };
     handleDelete = item => {
         axios
             .delete(`http://localhost:8000/api/todos/${item.id}/`)
-            .then(res => this.refreshList());
+            .then(res => this.refreshItemList());
     };
     createItem = () => {
         const item = {title: "", description: "", completed: false};
-        this.setState({activeItem: item, modal: !this.state.modal});
+        this.setState({activeItem: item, modalItem: !this.state.modalItem});
+    };
+    createOwner = () => {
+        const owner = {id:1, name: "", join_date: ""};
+        this.setState({activeOwner: owner, modalOwner: !this.state.modalOwner});
     };
     editItem = item => {
-        this.setState({activeItem: item, modal: !this.state.modal});
+        this.setState({activeItem: item, modalItem: !this.state.modalItem});
     };
 
     render() {
         return (
             <main className="content">
                 <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
+                {/* todoList Card */}
                 <div className="row ">
                     <div className="col-md-6 col-sm-10 mx-auto p-0">
                         <div className="card p-3">
@@ -133,11 +214,33 @@ class App extends Component {
                         </div>
                     </div>
                 </div>
-                {this.state.modal ? (
+                {/* Owner Card */}
+                <div className="row ">
+                    <div className="col-md-6 col-sm-10 mx-auto p-0">
+                        <div className="card p-3">
+                            <div className="">
+                                <button onClick={this.createOwner} className="btn btn-primary">
+                                    Add Owner
+                                </button>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                {this.renderOwners()}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                {this.state.modalItem ? (
                     <Modal
                         activeItem={this.state.activeItem}
-                        toggle={this.toggle}
-                        onSave={this.handleSubmit}
+                        toggle={this.toggleItem}
+                        onSave={this.handleSubmitItem}
+                    />
+                ) : null}
+                {this.state.modalOwner ? (
+                    <Modal
+                        activeItem={this.state.activeOwner}
+                        toggle={this.toggleOwner}
+                        onSave={this.handleSubmitOwner}
                     />
                 ) : null}
             </main>
